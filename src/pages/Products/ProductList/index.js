cbb9552a632459ee8d6b9c696c9eafba34136388
ViniBortoletto <ovinibortoletto@gmail.com>
2020-04-23 16:51:19 -0300

    add: display only 6 items in the product list | button on click adds 3 more

--- src/pages/Products/ProductList/index.js
+++ src/pages/Products/ProductList/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { ProductConsumer } from "../../../Context";
 
@@ -7,24 +7,41 @@ import ProductCard from "../../../components/ProductCard/index";
 
 // Styles
 import { Container } from "./styles";
+import { ButtonSquare } from "../../../components/Buttons/styles";
 
 export default function ProductList() {
+  const [itemsToShow, setItemsToShow] = useState(5);
+
+  function handleItemsToShow() {
+    setItemsToShow(itemsToShow + 3);
+  }
+
   return (
     <Container>
-      <ProductConsumer>
-        {(value) => {
-          return value.products.map((product) => (
-            <Link to="/detalhes-do-produto">
-              <ProductCard
-                key={product.sys.id}
-                img={product.fields.img.fields.file.url}
-                title={product.fields.title}
-                price={product.fields.price}
-              />
-            </Link>
-          ));
-        }}
-      </ProductConsumer>
+      <div className="products_list">
+        <ProductConsumer>
+          {(value) => {
+            return value.products.map(
+              (product, index) =>
+                index <= itemsToShow && (
+                  <Link key={product.sys.id} to="/detalhes-do-produto">
+                    <ProductCard
+                      img={product.fields.img.fields.file.url}
+                      title={product.fields.title}
+                      price={product.fields.price}
+                    />
+                  </Link>
+                )
+            );
+          }}
+        </ProductConsumer>
+      </div>
+
+      <div className="button_container">
+        <ButtonSquare mini transparent onClick={handleItemsToShow}>
+          ver mais produtos
+        </ButtonSquare>
+      </div>
     </Container>
   );
 }
--- src/pages/Products/ProductList/styles.js
+++ src/pages/Products/ProductList/styles.js
@@ -2,19 +2,29 @@ import styled from "styled-components";
 import { Breakpoint } from "../../../helpers/mixins";
 
 export const Container = styled.section`
-  width: 100%;
-
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(14rem, 1fr));
-  gap: var(--m_sm);
-  justify-items: center;
-
-  .product_card {
+  .products_list {
     width: 100%;
-    transition: 0.2s ease-in-out;
+    margin-bottom: var(--m_lg);
+
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(14rem, 1fr));
+    gap: var(--m_sm);
+    justify-items: center;
+
+    .product_card {
+      width: 100%;
+      transition: 0.2s ease-in-out;
+    }
+  }
+
+  .button_container {
+    width: 100%;
+    text-align: center;
   }
 
   @media ${Breakpoint("1200")} {
-    justify-items: right;
+    .products_list {
+      justify-items: right;
+    }
   }
 `;
