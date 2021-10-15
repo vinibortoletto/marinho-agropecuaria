aab9ed5b22c966f08db2ab86cd8b5e9209d76930
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2021-10-15 17:46:50 -0300

    Updates

---
+++ precache-manifest.1db968f9798d6a49b368a591bff3e7bc.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00e705b9e2bb09834520f851dfb2e9d8",
+    "url": "/marinho-agropecuaria/index.html"
+  },
+  {
+    "revision": "1474d82a1116666fa83f",
+    "url": "/marinho-agropecuaria/static/css/2.eccc2922.chunk.css"
+  },
+  {
+    "revision": "23197893d6687fe9332d",
+    "url": "/marinho-agropecuaria/static/css/main.709c718c.chunk.css"
+  },
+  {
+    "revision": "1474d82a1116666fa83f",
+    "url": "/marinho-agropecuaria/static/js/2.adb1096b.chunk.js"
+  },
+  {
+    "revision": "f483a56f1333ce84070992768491e145",
+    "url": "/marinho-agropecuaria/static/js/2.adb1096b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23197893d6687fe9332d",
+    "url": "/marinho-agropecuaria/static/js/main.a3849bb7.chunk.js"
+  },
+  {
+    "revision": "32524cb792f23bf1aa7f",
+    "url": "/marinho-agropecuaria/static/js/runtime-main.fff74f8d.js"
+  },
+  {
+    "revision": "43f00ad108ae64a855941a58067e8eb1",
+    "url": "/marinho-agropecuaria/static/media/404.43f00ad1.svg"
+  },
+  {
+    "revision": "64c248e4810717dc6f3f563b277af62b",
+    "url": "/marinho-agropecuaria/static/media/abcomm.64c248e4.png"
+  },
+  {
+    "revision": "4366827f87d8305c899392d00bc6dac3",
+    "url": "/marinho-agropecuaria/static/media/bird.4366827f.svg"
+  },
+  {
+    "revision": "4601a548e8a91f659b7f1410df687551",
+    "url": "/marinho-agropecuaria/static/media/bird.4601a548.svg"
+  },
+  {
+    "revision": "aa5a9a23001bf44ce9725bbeda97846e",
+    "url": "/marinho-agropecuaria/static/media/cat.aa5a9a23.svg"
+  },
+  {
+    "revision": "fbe1230e4c36360c7a96d3278eff0ff0",
+    "url": "/marinho-agropecuaria/static/media/cat.fbe1230e.svg"
+  },
+  {
+    "revision": "4a9abdf7512705b2e5e1e861e6f66fbf",
+    "url": "/marinho-agropecuaria/static/media/comodo.4a9abdf7.png"
+  },
+  {
+    "revision": "ae823064e9331fb1d3b5586e5d3ad100",
+    "url": "/marinho-agropecuaria/static/media/correios.ae823064.png"
+  },
+  {
+    "revision": "a8fe9a1a860b6fe863c569d6786ff259",
+    "url": "/marinho-agropecuaria/static/media/dog.a8fe9a1a.svg"
+  },
+  {
+    "revision": "cb1d5879ce8a4cee485ef5c85c8fcba6",
+    "url": "/marinho-agropecuaria/static/media/dog.cb1d5879.svg"
+  },
+  {
+    "revision": "a13fd387771a77ada041a09ea8bde3d2",
+    "url": "/marinho-agropecuaria/static/media/dogs-bg.a13fd387.jpg"
+  },
+  {
+    "revision": "95679171e38dd8fd919c1b54677f4963",
+    "url": "/marinho-agropecuaria/static/media/dogs-md.95679171.jpg"
+  },
+  {
+    "revision": "d004aace83995c4b9322e3fc40f0617b",
+    "url": "/marinho-agropecuaria/static/media/dogs-sm.d004aace.jpg"
+  },
+  {
+    "revision": "2da21e5a5cab0a65b8d58aaa8ab20c68",
+    "url": "/marinho-agropecuaria/static/media/ebit.2da21e5a.png"
+  },
+  {
+    "revision": "424fa108765d26c4167d8de70b4a808b",
+    "url": "/marinho-agropecuaria/static/media/fish.424fa108.svg"
+  },
+  {
+    "revision": "68ecb571e21ba9ea32fe76309cd8a581",
+    "url": "/marinho-agropecuaria/static/media/fish.68ecb571.svg"
+  },
+  {
+    "revision": "58183a49e9cad9f5a9fde586bdec733b",
+    "url": "/marinho-agropecuaria/static/media/garden-bg.58183a49.jpg"
+  },
+  {
+    "revision": "40971597cde8de3c637b211f396dc939",
+    "url": "/marinho-agropecuaria/static/media/garden-md.40971597.jpg"
+  },
+  {
+    "revision": "80f32237fe90849a4e9479c758392e45",
+    "url": "/marinho-agropecuaria/static/media/garden-sm.80f32237.jpg"
+  },
+  {
+    "revision": "96138805b5feac0fa8c295e30fee1116",
+    "url": "/marinho-agropecuaria/static/media/garden.96138805.svg"
+  },
+  {
+    "revision": "e717e47dc2994fae5e43d601de3eb6b6",
+    "url": "/marinho-agropecuaria/static/media/garden.e717e47d.svg"
+  },
+  {
+    "revision": "cfb21e21d337c5777ee1ac9db1e808eb",
+    "url": "/marinho-agropecuaria/static/media/house-bg.cfb21e21.jpg"
+  },
+  {
+    "revision": "20ba069445e6ba8d431fe8b54174e80e",
+    "url": "/marinho-agropecuaria/static/media/house-md.20ba0694.jpg"
+  },
+  {
+    "revision": "2c45fc000df8f1fa24d341b9bfa346fd",
+    "url": "/marinho-agropecuaria/static/media/house-sm.2c45fc00.jpg"
+  },
+  {
+    "revision": "554a55761a31e413f410f012dd4533cd",
+    "url": "/marinho-agropecuaria/static/media/house.554a5576.svg"
+  },
+  {
+    "revision": "a17e5e7ddabd1e4fc2032690793ab3e9",
+    "url": "/marinho-agropecuaria/static/media/house.a17e5e7d.svg"
+  },
+  {
+    "revision": "86515114a8af9de08db5544989ddb718",
+    "url": "/marinho-agropecuaria/static/media/info.86515114.svg"
+  },
+  {
+    "revision": "fd7f234f87b1f6f4b8da1bce915edebd",
+    "url": "/marinho-agropecuaria/static/media/info.fd7f234f.svg"
+  },
+  {
+    "revision": "d7755ca634f4707c7eee301b0974e3fa",
+    "url": "/marinho-agropecuaria/static/media/main.d7755ca6.svg"
+  },
+  {
+    "revision": "39d7237bbd7b1f028d70e3bbc38cf2cd",
+    "url": "/marinho-agropecuaria/static/media/man-bg.39d7237b.jpg"
+  },
+  {
+    "revision": "c0cfdfa82d1965b8a2ae89902532fff9",
+    "url": "/marinho-agropecuaria/static/media/man-md.c0cfdfa8.jpg"
+  },
+  {
+    "revision": "1a195816bf1d6df7c552640e9ae47f76",
+    "url": "/marinho-agropecuaria/static/media/man-sm.1a195816.jpg"
+  },
+  {
+    "revision": "76b8ba86b81bfbf62e73a382d36d5268",
+    "url": "/marinho-agropecuaria/static/media/pagseguro.76b8ba86.png"
+  },
+  {
+    "revision": "75b2b0098051d55ad0327ad07d52e349",
+    "url": "/marinho-agropecuaria/static/media/product_sample.75b2b009.png"
+  },
+  {
+    "revision": "1a54b3633a7a1a7062ee2cb0f3c68e9f",
+    "url": "/marinho-agropecuaria/static/media/reptile.1a54b363.svg"
+  },
+  {
+    "revision": "9c614722952692cba432f5f71d022d7f",
+    "url": "/marinho-agropecuaria/static/media/reptile.9c614722.svg"
+  },
+  {
+    "revision": "298411b94f91eb479b417dfecf19dc09",
+    "url": "/marinho-agropecuaria/static/media/rodent.298411b9.svg"
+  },
+  {
+    "revision": "5ae229c7e54bd5bb48ca6db8c545d6ea",
+    "url": "/marinho-agropecuaria/static/media/rodent.5ae229c7.svg"
+  }
+]);
